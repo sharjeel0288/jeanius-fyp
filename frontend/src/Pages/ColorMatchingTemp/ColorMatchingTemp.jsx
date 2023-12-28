e2373ba1b22b8d04c5cd138424996dc102b945c4
xHarry26 <shazimwork@gmail.com>
2023-12-28 06:11:50 +0500

    Color matching new temp

--- frontend/src/Pages/ColorMatchingTemp/ColorMatchingTemp.jsx
+++ frontend/src/Pages/ColorMatchingTemp/ColorMatchingTemp.jsx
@@ -1,9 +1,7 @@
 import { Box, Button, Container, Flex, Grid, Heading, Stack, Table, TableContainer, Tbody, Td, Text, Th, Thead, Tr } from '@chakra-ui/react';
 import React, { useState } from 'react';
 import Layout from './component/Layout';
-
-const ColorMatching = ({ sideBarWidth }) => {
-
+const ColorMatchingTemp = ({sideBarWidth}) => {
     return (
         // <Box minH="90vh" mt={10} maxW="90vw">
         <Box py={8} w="auto" minH="100vh">
@@ -13,13 +11,13 @@ const ColorMatching = ({ sideBarWidth }) => {
                     ml={{ base: 0, lg: sideBarWidth === "small" ? 14 : 60 }}
                     transition="margin 0.3s ease-in-out"
                 >
-                    <Heading>Color Matching - FUTURE</Heading>
+                    <Heading>Color Matching</Heading>
 
                     <Layout />
                 </Box>
             </Container >
         </Box >
     );
-};
+}
 
-export default ColorMatching;
+export default ColorMatchingTemp
